f29cd11d00517b6dec577e6d8f856bb3a66ff2fe
sawfiz <dashu.up@gmail.com>
2023-03-10 12:08:00 +0800

    Delete unneeded files from the root directory of the project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 import ScreenController from './screenController';
 
-
-
-// TODO: Add a form for game set up
-// Player attributes
-// Number of games to play
-
-
-
-// Prompt user for number of games to play
-// const numGames = parseInt(prompt('How many games do you want to play?'));
-
 // Create ScreenController instance and play game
 ScreenController();
